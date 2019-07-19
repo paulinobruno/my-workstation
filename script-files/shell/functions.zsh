30657b421fc89f3e7dfc879894736723b83a4250
Bruno Paulino <paulino.bruno@gmail.com>
2019-07-19 12:02:40 -0300

    :sparkles: add: pentaho starter

--- script-files/shell/functions.zsh
+++ script-files/shell/functions.zsh
@@ -6,3 +6,8 @@ host_for_container() {
   CONTAINER=$(docker ps | grep $TARGET | cut -c 1-12)
   docker inspect $CONTAINER | jq -r '.[0].NetworkSettings.Networks.docker_default.IPAddress'
 }
+
+pentaho() {
+  PENTAHO_HOME="$HOME/Apps/pentaho-data-integration"
+  sh $PENTAHO_HOME/spoon.sh &
+}
