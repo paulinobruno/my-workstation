04bdce85be51a5560ccba1548956277b174c2d24
Bruno Paulino <paulino.bruno@gmail.com>
2020-05-13 20:19:28 -0300

    :fire: remove: job related scripts

--- script-files/shell/functions.zsh
+++ script-files/shell/functions.zsh
@@ -11,17 +11,3 @@ pentaho() {
   PENTAHO_HOME="$HOME/Apps/pentaho-data-integration"
   sh $PENTAHO_HOME/spoon.sh &
 }
-
-kn() {
-  NS=$1
-
-  if [ "$NS" = "" ]; then
-    echo "Please specify the k8s namespace. One of:"
-    kubectl get namespaces
-  else
-    export K8S_NS=$NS
-    echo "K8s default namespace set to $K8S_NS."
-    echo "Please use k command to assume this default namespace"
-    alias k="kubectl -n $K8S_NS"
-  fi
-}
