2ba9e7a90657b0a01e911edd07adec56d58c137f
Bruno Paulino <paulino.bruno@gmail.com>
2020-09-25 19:24:46 -0300

    🔥 remove: useless function

--- script-files/shell/functions.zsh
+++ script-files/shell/functions.zsh
@@ -1,12 +1,5 @@
 #!/bin/zsh
 
-host_for_container() {
-  TARGET=$1
-
-  CONTAINER=$(docker ps | grep $TARGET | cut -c 1-12)
-  docker inspect $CONTAINER | jq -r '.[0].NetworkSettings.Networks.docker_default.IPAddress'
-}
-
 pentaho() {
   PENTAHO_HOME="$HOME/Apps/pentaho-data-integration"
   sh $PENTAHO_HOME/spoon.sh &
